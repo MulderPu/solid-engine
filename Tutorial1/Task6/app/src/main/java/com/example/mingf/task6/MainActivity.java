3995e03d6f4ec34ac8badfca65ed59631785d3fe
Mulder Pu <MulderPu@gmail.com>
2017-01-14 14:57:56 +0800

    added Task 6

--- Tutorial1/Task6/app/src/main/java/com/example/mingf/task6/MainActivity.java
+++ Tutorial1/Task6/app/src/main/java/com/example/mingf/task6/MainActivity.java
@@ -1,42 +1,29 @@
 package com.example.mingf.task6;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity implements View.OnClickListener {
     TextView text;
     Button btnTrue;
     Button btnFalse;
     Button btnNext;
-    boolean next = false;
+
     int i=0;
-
-
-    TextView text2;
-    TextView text3;
+    boolean ans;
+    boolean clicked = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        text = (TextView) findViewById(R.id.textView);
-        text.setText(mQuestionBank[i].getQuestion());
-        boolean ans = mQuestionBank[i].getB();
-
-
-        text2 = (TextView) findViewById(R.id.textView2);
-        if(ans == true) {
-            text2.setText("true");
-        }else{
-            text2.setText("false");
-        }
-
-        text = (TextView) findViewById(R.id.textView);
         btnTrue = (Button) findViewById(R.id.button);
         btnFalse = (Button) findViewById(R.id.button2);
         btnNext = (Button) findViewById(R.id.button3);
@@ -44,17 +31,13 @@ public class MainActivity extends Activity implements View.OnClickListener {
         btnFalse.setOnClickListener(this);
         btnNext.setOnClickListener(this);
 
-        text3 = (TextView) findViewById(R.id.textView3);
-        if(next == true){
-            text3.setText("true");
-        }else{
-            text3.setText("false");
-        }
+        text = (TextView) findViewById(R.id.textView);
+        text.setText(mQuestionBank[i].getQuestion());
 
     }
 
     //array of objects to store questions and answer
-    private Question[] mQuestionBank = new Question[] {
+    public Question[] mQuestionBank = new Question[] {
             new Question(R.string.question_oceans, true),
             new Question(R.string.question_mideast, false),
             new Question(R.string.question_africa, false),
@@ -66,17 +49,79 @@ public class MainActivity extends Activity implements View.OnClickListener {
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-
             case R.id.button:
+                ans = mQuestionBank[i].getB();
+                clicked = true;
+                //display in short period of time
+//                Toast.makeText(getApplicationContext(), "True clicked.",
+//                        Toast.LENGTH_SHORT).show();
 
+                if( ans == true){
+
+                    Snackbar.make(findViewById(android.R.id.content), "CORRECT !", Snackbar.LENGTH_LONG)
+                            .setAction("Undo", new View.OnClickListener(){
+                                @Override
+                                public void onClick(View view) {
+                                }
+                            })
+                            .setActionTextColor(Color.GREEN)
+                            .show();
+
+                }else{
+                    Snackbar.make(findViewById(android.R.id.content), "INCORRECT !", Snackbar.LENGTH_LONG)
+                            .setAction("Undo", new View.OnClickListener(){
+                                @Override
+                                public void onClick(View view) {
+                                }
+                            })
+                            .setActionTextColor(Color.GREEN)
+                            .show();
+                }
                 break;
 
             case R.id.button2:
+                ans = mQuestionBank[i].getB();
+                clicked = true;
+                //display in short period of time
+//                Toast.makeText(getApplicationContext(), "False clicked.",
+//                        Toast.LENGTH_SHORT).show();
 
+                if( ans == false){
+
+                    Snackbar.make(findViewById(android.R.id.content), "CORRECT !", Snackbar.LENGTH_LONG)
+                            .setAction("Undo", new View.OnClickListener(){
+                                @Override
+                                public void onClick(View view) {
+                                }
+                            })
+                            .setActionTextColor(Color.GREEN)
+                            .show();
+
+                }else{
+                    Snackbar.make(findViewById(android.R.id.content), "INCORRECT !", Snackbar.LENGTH_LONG)
+                            .setAction("Undo", new View.OnClickListener(){
+                                @Override
+                                public void onClick(View view) {
+                                }
+                            })
+                            .setActionTextColor(Color.GREEN)
+                            .show();
+                }
                 break;
 
             case R.id.button3:
-                next = true;
+
+                    i++;
+                    text.setText(mQuestionBank[i].getQuestion());
+                    ans = mQuestionBank[i].getB();
+
+                    if(Integer.toString(i) == "4"){
+                        Toast.makeText(getApplicationContext(), "End of Questions.",
+                                Toast.LENGTH_SHORT).show();
+                        btnNext.setEnabled(false);
+
+                    }
+
                 break;
 
             default:
