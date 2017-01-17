4cf073a8d3e2865665f3ecb23953712668686c26
Mulder Pu <MulderPu@gmail.com>
2017-01-17 23:32:49 +0800

    changed lightme code to better practices

--- Tutorial2/LightMe/app/src/main/java/com/example/mingf/lightme/LightMeActivity.java
+++ Tutorial2/LightMe/app/src/main/java/com/example/mingf/lightme/LightMeActivity.java
@@ -1,7 +1,6 @@
 package com.example.mingf.lightme;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
@@ -16,38 +15,27 @@ public class LightMeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_light_me);
-
-        init(savedInstanceState);
+        init();
     }
 
-    private void init(Bundle savedInstanceState) {
+    private void init() {
         offImageView = (ImageView) findViewById(R.id.offImageView);
         offImageView.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
-                ImageView imageView = (ImageView) view;
-                if(motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
-                    // Button was pressed, change button background
-                    imageView.setImageResource(R.drawable.on);
-                    return true;
-                } else if(motionEvent.getAction() == MotionEvent.ACTION_UP) {
-                    // Button was released, reset button background
-                    imageView.setImageResource(R.drawable.off);
-                    return true;
+                switch (motionEvent.getAction() & MotionEvent.ACTION_MASK) {
+                    case MotionEvent.ACTION_DOWN:
+                        view.setPressed(true);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        view.setPressed(false);
+                        break;
                 }
 
-                return false;
+                return true;
             }
         });
 
-        if( savedInstanceState != null){
-            image = savedInstanceState.getInt("image", R.drawable.on);
-        }
     }
 
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putInt("image", R.drawable.on );
-    }
 }
