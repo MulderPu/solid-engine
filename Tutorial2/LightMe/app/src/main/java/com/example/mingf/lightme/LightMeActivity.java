3139c436f92999ba6fb6c259c6d62fcf765c291e
Mulder Pu <MulderPu@gmail.com>
2017-01-17 22:54:56 +0800

    added lightme

--- Tutorial2/LightMe/app/src/main/java/com/example/mingf/lightme/LightMeActivity.java
+++ Tutorial2/LightMe/app/src/main/java/com/example/mingf/lightme/LightMeActivity.java
@@ -1,29 +1,53 @@
 package com.example.mingf.lightme;
 
-import android.media.Image;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 
 public class LightMeActivity extends AppCompatActivity {
 
+    ImageView offImageView;
+    public int image;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_light_me);
+
+        init(savedInstanceState);
     }
 
-    public void toggleBulb(View view) {
-        ImageView onImageView = (ImageView) findViewById(R.id.onImageView);
-        ImageView offImageView = (ImageView) findViewById(R.id.offImageView);
+    private void init(Bundle savedInstanceState) {
+        offImageView = (ImageView) findViewById(R.id.offImageView);
+        offImageView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                ImageView imageView = (ImageView) view;
+                if(motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                    // Button was pressed, change button background
+                    imageView.setImageResource(R.drawable.on);
+                    return true;
+                } else if(motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                    // Button was released, reset button background
+                    imageView.setImageResource(R.drawable.off);
+                    return true;
+                }
 
-        if(onImageView.getVisibility() == View.VISIBLE){
-            onImageView.setVisibility(View.GONE);
-            offImageView.setVisibility(View.VISIBLE);
-        }else{
-            offImageView.setVisibility(View.GONE);
-            onImageView.setVisibility(View.VISIBLE);
+                return false;
+            }
+        });
+
+        if( savedInstanceState != null){
+            image = savedInstanceState.getInt("image", R.drawable.on);
         }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("image", R.drawable.on );
     }
 }
