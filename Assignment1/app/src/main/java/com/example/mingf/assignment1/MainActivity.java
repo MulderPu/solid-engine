1a960699e6ab09520108806f3ab2e5289c076a8a
Mulder Pu <MulderPu@gmail.com>
2017-01-26 21:00:46 +0800

    complete portait assignment 1

--- Assignment1/app/src/main/java/com/example/mingf/assignment1/MainActivity.java
+++ Assignment1/app/src/main/java/com/example/mingf/assignment1/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.mingf.assignment1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textViewPersonality;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String text = "Mike Wazowski is a proud and confident monster. Mike has a romantic side" +
+                ", shown by his dating Celia Mae, presumably the one Mike wants to tie the knot with," +
+                "and refers to as his " + "<b>" + "schmoopsie-poo" + "</b>" + " although he has a self-centered side";
+        textViewPersonality = (TextView) findViewById(R.id.textViewPersonality2);
+        textViewPersonality.setText(Html.fromHtml(text));
     }
 }
